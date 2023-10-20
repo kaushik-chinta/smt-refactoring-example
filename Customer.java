67a58a308738102908ff1899e50348c326e10231
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2023-10-20 18:20:10 +0100

    Extract the calculation of the amount

--- Customer.java
+++ Customer.java
@@ -23,25 +23,7 @@ public class Customer {
         String result = "\nRental Record for " + getName() + "\n";
         for (Rental each : rentals) {
 
-            // Determine amounts for each line
-            double thisAmount = 0;
-            switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-                break;
-            }
+            double thisAmount = amountFor(each);
 
             // Add frequent renter points
             frequentRenterPoints++;
@@ -60,4 +42,26 @@ public class Customer {
         return result;
     }
 
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2) {
+                thisAmount += (each.getDaysRented() - 2) * 1.5;
+            }
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3) {
+                thisAmount += (each.getDaysRented() - 3) * 1.5;
+            }
+            break;
+        }
+        
+        return thisAmount;
+    }
 }
