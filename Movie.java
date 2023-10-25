bca1abd4fa4ce45a2ffa2a2eaa1e3cb5ca0239cc
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2023-10-25 18:06:56 +0100

    delegate getFrequentRenterPoints

--- Movie.java
+++ Movie.java
@@ -38,10 +38,6 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2 ;
-        }
-
-        return 1;
+        return calculator.getFrequentRenterPoints(daysRented);
     }
 }
--- PriceCalculator.java
+++ PriceCalculator.java
@@ -3,4 +3,12 @@ public abstract class PriceCalculator {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2 ;
+        }
+
+        return 1;
+    }
 }
