f99c66c115df701a2c107f21b102e2337e1ad47a
Kaushik Chinta <kaushik.chinta@kcl.ac.uk>
2023-10-25 18:08:47 +0100

    Add override for  frequent pointer

--- NewReleasePriceCalculator.java
+++ NewReleasePriceCalculator.java
@@ -8,4 +8,13 @@ public class NewReleasePriceCalculator extends PriceCalculator {
     public double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1) {
+            return 2;
+        }
+
+        return 1;
+    }
 }
--- PriceCalculator.java
+++ PriceCalculator.java
@@ -5,10 +5,6 @@ public abstract class PriceCalculator {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2 ;
-        }
-
         return 1;
     }
 }
